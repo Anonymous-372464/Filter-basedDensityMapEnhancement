ab0585586dd8308f3b18e3361fccea7d0290be92
Anonymous-372464 <bhuaiwei@gmail.com>
2024-10-24 20:39:16 +0800

    update index

--- dist2/assets/index.ba022ecc.js
+++ dist2/assets/index.ba022ecc.js
@@ -23342,8 +23342,8 @@ class W$ {
   }
   load() {
     if (this.worker) return new Promise((n) => n());
-    const t = "/Filter-basedDensityMapEnhancement"
-    (import.meta.url.includes("src") ? "/src" : "");
+    const t = "/Filter-basedDensityMapEnhancement/"
+    (import.meta.url.includes("src") ? "src" : "");
     return (
       (this.worker = new Worker(`${t}/dist2/assets/worker/cv.worker.js`)),
       this.load2DColormap(t),
